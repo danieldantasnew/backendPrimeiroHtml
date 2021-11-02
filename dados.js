77ffc344e260b4427783d51abcc7eed07eff48de
Daniel Dantas <xxxxxdanielzimmxxxx.danieldant@gmail.com>
2021-11-02 19:56:14 -0300

    alterando dados

---
+++ dados.js
@@ -0,0 +1,39 @@
+module.exports = [
+    {
+        id: "lle0NNmvIyU",
+        tittle: "Star Wars: Visions | Original Trailer | Disney+",
+        duration: "1:55 Min",
+        price: "Grátis"
+    },
+    {
+        id: "jjpe0nOsXVo",
+        tittle: "“Of the Dark Side” Official Clip | LEGO Star Wars Terrifying Tales | Disney+",
+        duration: "0:56 Min",
+        price: "Grátis"
+    },
+    {
+        id: "cSKg7trjhcc",
+        tittle: "Mercenaries | Star Wars: The Bad Batch | Disney+",
+        duration: "0:30 Min",
+        price: "Grátis"
+    },
+    {
+        id: "lJQwSrryOV8",
+        tittle: "LEGO Makes R2-D2 Even Cuter, Dok-Ondar Stops In For a Drink, and More!",
+        duration: "3:58 Min",
+        price: "Grátis"
+    },
+    {
+        id: "TPeRcyJE5dk",
+        tittle: "Sskeer | Characters of Star Wars the High Republic",
+        duration: "1:20 Min",
+        price: "Grátis"
+    },
+    {
+        id: "tGk3PkqLOOM",
+        tittle: "Season 2 Recap Sizzle | The Mandalorian | Disney+",
+        duration: "3:10 Min",
+        price: "Grátis"
+    },
+
+]
--- server.js
+++ server.js
@@ -2,23 +2,37 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
-
+const videos = require("./dados")
 
 server.use(express.static("public"))
-server.set("view engine", "html")
+server.set("view engine", "njk")
 
 
 nunjucks.configure("views",{
-    express:server
+    express:server,
+    autoescape: false
 })
 
 server.get("/",function(req, res){
-    return res.render("home")
+    const home = {
+        avatar_url: "https://yt3.ggpht.com/ytc/AKedOLTlsg15b-HJ3OmlIDy0xDPanZzTLOmDpbzXLCzn=s900-c-k-c0x00ffffff-no-rj",
+        title1: "STAR WARS",
+        title2: "História:",
+        description: 'A série teve início com o simples título Star Wars, escrito e dirigido por George Lucas, lançado em 25 de maio de 1977. Na época da sua estreia se tornou a maior bilheteria de todos os tempos, arrecadando US$ 775 398 007 milhões de dólares e ganhando sete prêmios no Óscar. A 20th Century Fox desacreditando um filme que ambientado no espaço, permitiu que George Lucas tivesse todos os direitos do filme. O sucesso garantiu a ele dinheiro suficiente para abrir sua própria empresa cinematográfica: a Lucasfilm e, o filme foi transformado em uma franquia e série, ganhando produtos derivados.',
+
+        links: [
+            {name: "facebook ", url: "https://www.facebook.com/StarWars.br/"},
+            {name: "twitter", url: "https://twitter.com/starwars"},
+            {name: "instagram", url: "https://www.instagram.com/starwars/"}
+        ]
+    }
+
+    return res.render("home", {home: home})
 })
 
 
 server.get("/programacao",function(req, res){
-    return res.render("programacao")
+    return res.render("programacao", {items: videos})
 })
 
 
